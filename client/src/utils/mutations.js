27b2cbed390e8085b65c5a4b102accf66b1f4334
canslercp <canslercp@gmail.com>
2022-08-26 16:57:14 -0400

    finished mutations and App.js

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -14,13 +14,32 @@ export const LOGIN_USER = gql`
 
 export const ADD_USER = gql`
     mutation addUsser($username: String!, $email: String!, $password: String!){
-        addUsser($username: String!, $email: String!, $password: String!) {
+        addUser($username: String!, $email: String!, $password: String!) {
             token
             user {
                 _id
-                name
+                username
             }
         }
     }
 `;
 
+export const SAVE_BOOK = gql`
+    mutation saveBook($user: ID!, $savedBook: String!){
+        saveBook(userId: $userId, savedBook: $savedBook){
+            _id
+            username
+            savedBooks
+        }
+    }     
+`
+
+export const REMOVE_BOOK = gql`
+    mutation removeBook($savedBook: String!){
+        removeBook(savedBook: $savedBook){
+            _id
+            username
+            savedBooks
+        }
+    }
+`
